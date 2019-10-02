803a5467d4305c599bdece8fa62e62017e4ae869
RZQ456 <54938743+RZQ456@users.noreply.github.com>
2019-10-02 17:55:46 +0800

    Add files via upload

---
+++ 课件包/scr/main/Test2.java
@@ -0,0 +1,11 @@
+package main;
+import course.Course;
+import student.Student;
+import teacher.Teacher;
+public class Test2{
+	public static void main(String[] args){
+		Course cou=new Course();
+		Student stu=new Student();
+		Teacher tea=new Teacher();
+	}
+}
